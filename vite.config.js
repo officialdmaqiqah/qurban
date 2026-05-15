020e418e5ea4e6f087b4a3530744ffde600ecc65
DM Aqiqah Official <officialdmaqiqah@gmail.com>
2026-05-15 21:20:30 +0700

    fix: smart sync v11.17 - terpadu sulam and dropdown fix

--- vite.config.js
+++ vite.config.js
@@ -31,7 +31,8 @@ export default defineConfig({
         mega_sync: resolve(__dirname, 'mega_sync_v1113.html'),
         mega_sync_v1114: resolve(__dirname, 'mega_sync_v1114.html'),
         mega_sync_v1115: resolve(__dirname, 'mega_sync_v1115.html'),
-        mega_sync_v1116: resolve(__dirname, 'mega_sync_v1116.html')
+        mega_sync_v1116: resolve(__dirname, 'mega_sync_v1116.html'),
+        mega_sync_v1117: resolve(__dirname, 'mega_sync_v1117.html')
       }
     }
   },
